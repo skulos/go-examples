d77803bfdf29817ef46d1356e4cd4f5a715e91bc
Hendre Hayman <55100086+skulos@users.noreply.github.com>
2024-08-23 22:42:26 +0200

    first lession

---
+++ 1. Introduction to Programming Concepts/1.2 Variables and Data Types/main.go
@@ -0,0 +1,31 @@
+package main
+
+import "fmt"
+
+func main() {
+	var variable1 int = -100 // 0 1 2 3 4 5
+
+	var variable2 string = "I love New York" // "abc" ""
+
+	var variable3 float64 = 3.14 // 0 3.14
+
+	var variable4 bool = true // true false
+
+	fmt.Println(variable1)
+	fmt.Println(variable4)
+	fmt.Println(variable3)
+	fmt.Println(variable2)
+
+	// var newVariable int = 100
+	// newVariable := 100
+
+	// var router GinGonicServer.Router = ...
+	// router := ...
+
+	var a, b, c int
+
+	a, b, c = 1, 1, 1
+	// a = 1
+	// b = 1
+	// c = 1
+}
